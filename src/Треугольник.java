09d2015fb28566d862ed9701504c949434b7ef22
Zayniddin <tuit0778@gmail.com>
2024-12-05 14:27:12 +0500

    triugolnik)

---
+++ src/Треугольник.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Треугольник {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input1 = Integer.parseInt(reader.readLine());
+        int input2 = Integer.parseInt(reader.readLine());
+        int input3 = Integer.parseInt(reader.readLine());
+
+
+        if (input1 + input2 > input3 && input2 + input3 > input1 && input1 + input3 > input2)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+        reader.close();
+        writer.close();
+    }
+}
